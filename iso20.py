71792c9f671e8ece9f4d4d1745df04d0051c883f
SebastianMckenna <48081183+SebastianMckenna@users.noreply.github.com>
2019-03-04 14:08:45 +1100

    Add files via upload

--- iso20.py
+++ iso20.py
@@ -1,53 +1,111 @@
 import xarray as xr
 import dask
+from dask.diagnostics import ProgressBar
 import pandas as pd
 import numpy as np
 import scipy
+from scipy import signal
 import matplotlib.pyplot as plt
+from scipy.stats import skew
+from scipy.stats import kurtosis
+from scipy.stats import ttest_1samp
 import cartopy.crs as ccrs
 import datetime
 import matplotlib.lines as mlines
 import numpy.ma as ma
 ocean=xr.open_dataset('thetao_.nc')
 ocean_lon = ocean.sel(lat=slice(-10,0)).mean(['lat'])
-#note this dataset is was remapped and latitude limted in CDO 
-#it was also merged with all other times. 
+
+
+def isolevel(lev1, lev2, t1, t2, t=293.15):
+    return (lev1 + (t-t1)*((lev2-lev1)/(t2-t1)))
+
+
+
 """
-this is shell script used to create the set
-module load cdo
-cdo -mergetime /g/data1/rr3/publications/CMIP5/output1/CSIRO-BOM/ACCESS1-0/historical/mon/ocean/Omon/r1i1p1/latest/thetao/*.nc /short/e14/sm2435/thetao.nc
-cdo -sellevidx,1/24 -sellonlatbox,0,360,-10,10 -remapbil,global_1 /short/e14/sm2435/thetao.nc /short/e14/sm2435/thetao_.nc
+for i, l in ocean_lon[i]
+    iso=isolevel(ocean_lon[i, l].lev, ocean_lon[i, l+1].lev, ocean_lon[i, l], ocean_lon[i, l+1])
+print iso
 """
-#find where the 20 degree isotherm lies at a set point in lon and lat
+#take vector at time=0, lat = -5 lon = 75
+OV = ocean.thetao[:,:,:,:]
+#print OV
+#plt.figure(1)
+#plt.plot(OV.values, OV.lev)
+#plt.gca().invert_yaxis()
+#plt.show()
+
+#this is wrong, as the isolevel is not always between these levels
+iso= isolevel(OV[:,12].lev, OV[:,13].lev, OV[:,12], OV[:,13])
+plt.figure(1)
+plt.pcolormesh(iso[0], vmin=90, vmax=300)
+plt.colorbar()
+
+OV=ocean.thetao[:,:,5,75]
+print OV 
+
+OV=ocean.thetao[:,:,5,75]
+
+
+
+
 #create empty array to output data into, this will be good for one point
 level=xr.DataArray(np.zeros((len(ocean.time))), dims=['time'], coords={'time': ocean.time})
 level[:]=np.nan
-#this loop goes through every timestep for this exact point( 75E, 5S) and interpolates the 20 degree isotherm level.
+#this loop goes through every timestep for this exact point and interpolates the 20 degree isotherm level.
 for i, t in enumerate (ocean.thetao.time):
     print (i, t.values)
-    OV=ocean.thetao[i,:,5,75]#at a specific lat lon
-    T=np.sort(OV.values)#sorts the array so it is increasing (neded for interp function)
-    lev=np.array(OV.lev)[np.argsort(OV.values)]#indexes the level based on the above sorting
-    level[i] = float(np.interp(293.15, T, lev))#interpolates level and outputs into array created above
-
-print level
-#look at changes through time of isotherm level at this point
-plt.figure(1)
-plt.plot(level)
-plt.show()
-
-#now  do this loop over every lat lon to get surface of the level of 20 deg isotherm
-#create new array with time lon lat for outputs
-level=xr.DataArray(np.zeros((len(ocean.time), len(ocean.lon), len(ocean.lat))), dims=['time','lon','lat'], coords={'time': ocean.time, 'lon': ocean.lon, 'lat': ocean.lat})
-level[:]=np.nan
-
-#run the same loop but at all points - this does not work and gives an error
-for i, t, in enumerate (ocean.thetao.time):
-    print (i, t.values)
-    OV=ocean.thetao[i,:,:,:]
+    OV=ocean.thetao[i,:,5,75]
     T=np.sort(OV.values)
     lev=np.array(OV.lev)[np.argsort(OV.values)]
     level[i] = float(np.interp(293.15, T, lev))
 
+print level
+#look at changes through time of isotherm level at this point
+plt.figure(2)
+plt.plot(level)
+#plt.show()
+
+#now try for longtudinal profile of the level
+
+
+
+level=xr.DataArray(np.zeros((len(ocean.time), len(ocean.lon), len(ocean.lat))), dims=['time','lon','lat'], coords={'time': ocean.time, 'lon': ocean.lon, 'lat': ocean.lat})
+level[:]=np.nan
+
+"""
+for i, t, in enumerate (ocean.thetao.time):
+    print (i, t.values)
+    OV=ocean.thetao[i,:,:,:]
+    T=np.sort(OV.values, axis=1)
+    lev=np.array(OV.lev)[np.argsort(OV.values,axis=1)]
+    level[i] = float(np.interp(293.15, T, lev))
+
+print level
+"""
+with ProgressBar():
+    for j, t, in enumerate (ocean.lon):
+        for k, t in enumerate (ocean.lat):
+            for i, t, in enumerate (ocean.thetao.time):               
+                OV=ocean.thetao[i,:,k,j]
+                T=np.sort(OV.values)
+                lev=np.array(OV.lev)[np.argsort(OV.values)]
+                level[i,j,k] = float(np.interp(293.15, T, lev))
+
+print level
+
+plt.figure(3)
+plt.plot(level.mean('time'))
+plt.show()
+"""
 
  
+
+
+ocean_lon=ocean_lon.thetao[0]
+T=np.sort(ocean_lon.values, axis=1)
+print T
+lev=np.array(ocean_lon.lev)[np.argsort(ocean_lon.values, axis = 1)]
+print lev
+print np.interp(293.15, T, lev)
+"""
